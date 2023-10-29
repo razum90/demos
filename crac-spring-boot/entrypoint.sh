5959b63e794fa7ea17e27cb5285a953e743e9d97
Rasmus Kockum <rasmus@transcendsoftware.se>
2023-10-29 23:21:10 +0100

    WIP

--- crac-spring-boot/entrypoint.sh
+++ crac-spring-boot/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-# Here, simply start your application
-java -Xmx1024m -Dspring.context.checkpoint=onRefresh -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/app.jar
+
+if [ -z "$(ls -A $CRAC_FILES_DIR)" ]; then
+  echo 128 > /proc/sys/kernel/ns_last_pid; java -Dspring.context.checkpoint=onRefresh -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/app.jar
+else
+  java -XX:CRaCRestoreFrom=$CRAC_FILES_DIR&
+  PID=$!
+  trap "kill $PID" SIGINT SIGTERM
+  wait $PID
+fi
